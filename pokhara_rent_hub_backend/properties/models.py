9ca38f1739dcab533fd68568cfd7a1bafcfcf17b
preachya <preachyamulmi@gmail.com>
2025-04-29 18:46:33 +0545

    Add pokhara_rent_hub_backend/properties/models.py 1745931692296

---
+++ pokhara_rent_hub_backend/properties/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+class Property(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='properties')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100, default='Pokhara')
+    image = models.ImageField(upload_to='property_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
