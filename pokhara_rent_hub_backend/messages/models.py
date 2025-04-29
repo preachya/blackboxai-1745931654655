b7d1e2912d41817afcccc5b317dbe59f7348cd3a
preachya <preachyamulmi@gmail.com>
2025-04-29 18:47:03 +0545

    Add pokhara_rent_hub_backend/messages/models.py 1745931722226

---
+++ pokhara_rent_hub_backend/messages/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+
+class Message(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    is_chatbot = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender} to {self.receiver} at {self.timestamp}"
